50f4e5d22e3a2a17cb690ceafae512a3cc8430d7
James Gifford <jg.webdeveloper@gmail.com>
2015-08-30 23:53:51 -0700

    Content updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,210 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <title>Driver Signup Bonus</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta charset="utf-8">
+        <meta name="author" content="Roman Kirichik">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style-responsive.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/vertical-rhythm.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">  
+        
     </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5!</div>
-            </div>
+
+    <body class="appear-animate">
+        
+        <!-- Page Loader -->        
+        <div class="page-loader">
+            <div class="loader">Loading...</div>
         </div>
+        <!-- End Page Loader -->
+        
+        <!-- Page Wrap -->
+        <div class="page" id="top">
+            
+            <!-- Home Section -->
+            <section class="home-section bg-dark" data-background="video/video.jpg" id="home">
+                <div class="js-height-full container">
+                    
+                    <!-- Video BG Init -->
+                    <!-- Please, replace three video files in folder "video" with your own ones -->
+                    <div class="bg-video-wrapper">
+                        <div class="bg-video-overlay bg-dark-alfa-50"></div>
+                    </div>
+                     <!-- End Video BG Init -->
+                    
+                    <!-- Hero Content -->
+                    <div class="home-content">
+                        <div class="home-text">
+                            
+                            <h1 class="hs-line-8 font-alt mb-50 mb-xs-30">
+                                Driver Signup Bonus
+                            </h1>
+                            
+                            <h2 class="hs-line-14 font-alt mb-50 mb-xs-30">
+                                <span class="text-rotate">
+                                    Drive with Uber,
+                                    Drive with Lyft
+                                </span>
+                            </h2>
+                            
+                            <div class="local-scroll">
+                                <a href="#uber" class="btn btn-mod btn-border-w btn-medium btn-round hidden-xs">Drive with Uber</a>
+                                <span class="hidden-xs">&nbsp;</span>
+                                <a href="#lyft" class="btn btn-mod btn-border-w btn-medium btn-round hidden-xs">Drive with Lyft</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!-- End Hero Content -->
+                    
+                    <!-- Scroll Down -->
+                    <div class="local-scroll">
+                        <a href="#about" class="scroll-down"><i class="fa fa-angle-down scroll-down-icon"></i></a>
+                    </div>
+                    <!-- End Scroll Down -->
+                    
+                </div>
+            </section>
+            <!-- End Home Section -->
+            
+            <!-- Uber Section -->
+            <section class="page-section" id="uber">
+                <div class="container relative">
+                    
+                    <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
+                        Drive With Uber
+                    </h2>
+                    
+                    <div class="section-text mb-50 mb-sm-20">
+                        <div class="row">
+                        
+                            <div class="col-md-12">
+                                <blockquote>
+                                    <p>
+                                    Drive With Uber
+                                    </p>
+                                </blockquote>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        
+                    
+                    </div>
+                    
+                    
+                </div>
+            </section>
+            <!-- End Uber Section -->
+
+            <!-- Lyft Section -->
+            <section class="page-section" id="lyft">
+                <div class="container relative">
+                    
+                    <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
+                        Drive With Lyft
+                    </h2>
+                    
+                    <div class="section-text mb-50 mb-sm-20">
+                        <div class="row">
+                        
+                            <div class="col-md-12">
+                                <blockquote>
+                                    <p>
+                                    Drive With Lyft
+                                    </p>
+                                </blockquote>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        
+                    
+                    </div>
+                    
+                    
+                </div>
+            </section>
+            <!-- End Lyft Section -->
+            
+            <!-- Foter -->
+            <footer class="page-section bg-gray-lighter footer pb-60">
+                <div class="container">
+                    
+                    <!-- Footer Text -->
+                    <div class="footer-text">
+                        
+                        <div class="footer-made">
+                            Driver Signup Bonus
+                        </div>
+                        
+                    </div>
+                    <!-- End Footer Text --> 
+                    
+                 </div>
+                 
+                 
+                 <!-- Top Link -->
+                 <div class="local-scroll">
+                     <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+                 </div>
+                 <!-- End Top Link -->
+                 
+            </footer>
+            <!-- End Foter -->
+        
+        
+        </div>
+        <!-- End Page Wrap -->
+        
+        
+        <!-- JS -->
+        <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>        
+        <script type="text/javascript" src="js/SmoothScroll.js"></script>
+        <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+        <script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
+        <script type="text/javascript" src="js/jquery.viewport.mini.js"></script>
+        <script type="text/javascript" src="js/jquery.countTo.js"></script>
+        <script type="text/javascript" src="js/jquery.appear.js"></script>
+        <script type="text/javascript" src="js/jquery.sticky.js"></script>
+        <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.fitvids.js"></script>
+        <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+        <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+        <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+        <script type="text/javascript" src="js/gmap3.min.js"></script>
+        <script type="text/javascript" src="js/wow.min.js"></script>
+        <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+        <script type="text/javascript" src="js/jquery.simple-text-rotator.min.js"></script>
+        <script type="text/javascript" src="js/all.js"></script>
+        <script type="text/javascript" src="js/contact-form.js"></script>
+        <script type="text/javascript" src="js/jquery.ajaxchimp.min.js"></script>      
+        <script type="text/javascript" src="js/jquery.backgroundvideo.min.js"></script>   
+        <!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+        
     </body>
 </html>
